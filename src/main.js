8c22bba74f7cd8bc3c5f9cfafff97e74e6d3a2d0
ghson <ghson@ocube.co.kr>
2024-11-12 10:38:05 +0900

    적, 총알이랑 충돌, 우주선이랑 충돌 이벤트 추가

--- src/main.js
+++ src/main.js
@@ -26,15 +26,19 @@ const config = {
 
 const game = new Phaser.Game(config);
 let lastFired = 0;
+let score = 0;
+let gameScene;
 
 function preload() {
   // 리소스 로드
   this.load.image("player", "./assets/player.png");
   this.load.image("bullet", "./assets/bullet.png");
+  this.load.image("enemy", "./assets/enemy.png");
 }
 
 function create() {
   // 초기화 설정
+  gameScene = this;
   this.input.keyboard.on("keydown-F", () => {
     if (!this.scale.isFullscreen) {
       this.scale.startFullscreen();
@@ -57,18 +61,34 @@ function create() {
   player.setCollideWorldBounds(true); // 화면 밖으로 나가지 않게 설정
   player.setDisplaySize(100, 100);
 
+  this.enemies = this.physics.add.group({
+    key: "enemy",
+    repeat: 20, // 적 개수
+    setXY: { x: 100, y: this.cameras.main.height }, // 시작 위치와 간격
+  });
+
+  this.enemies.children.iterate((enemy) => {
+    enemy.setDisplaySize(40, 40);
+    enemy.setVelocityY(100);
+  });
+
   // 키보드 입력 설정
   this.cursors = this.input.keyboard.createCursorKeys();
   this.zKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
+  this.bullets = this.physics.add.group();
 
   this.fireBullet = () => {
     const bullet = this.physics.add.sprite(player.x, player.y - 40, "bullet");
     bullet.setActive(true);
     bullet.setVisible(true);
+    bullet.setCircle(5);
     bullet.body.velocity.y = -1000;
     bullet.checkWorldBounds = true;
     bullet.outOfBoundsKill = true;
+    this.physics.add.collider(bullet, this.enemies, destroyEnemy, null, this);
   };
+
+  this.physics.add.collider(player, this.enemies, loseGame, null, this);
 }
 
 function update(time) {
@@ -89,8 +109,34 @@ function update(time) {
     player.setVelocityY(0);
   }
 
+  this.enemies.children.iterate((enemy) => {
+    if (enemy.y > this.cameras.main.height) {
+      enemy.y = -50;
+      enemy.x = Phaser.Math.Between(50, this.cameras.main.width - 50);
+    }
+  });
+
   if (Phaser.Input.Keyboard.JustDown(this.zKey) && time > lastFired) {
     this.fireBullet();
     lastFired = time + 50;
   }
+}
+
+function destroyEnemy(bullet, enemy) {
+  bullet.destroy();
+  enemy.destroy();
+  score += 100;
+}
+
+function loseGame(player, enemy) {
+  player.destroy();
+  gameScene.scene.pause();
+  Toastify(
+    Object.assign({
+      text: `졌다! ${score}점!`,
+    })
+  ).showToast();
+  setTimeout(() => {
+    window.location.reload();
+  }, 3000);
 }
